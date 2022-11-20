70317023cb1512f914e99cd2f523f5c81bb65476
Tobiloba Osoba <tobiosoba777@gmail.com>
2022-11-20 23:50:43 +0100

    update

---
+++ Convert image color.py
@@ -0,0 +1,13 @@
+#import the required library
+
+import cv2
+#reading the image
+img = cv2.imread("car2.png")
+#if we display this image using imshow() function, then we can see that this image is in BGR format
+#Now, use cvtColor() function to convert this image to grayscale
+
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imshow('gray_car', gray)
+
+
+cv2.imshow("original image", img)
---
+++ edge detection.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 20 16:10:38 2022
+
+@author: TOBBY
+"""
+
+"""
+Humans, after seeing a rough sketch, can easily recognize many object types and their poses. 
+That is why edges play an important role in the life of humans as well as in the applications of computer vision.
+ OpenCV provides very simple and useful function called Canny()for detecting the edges.
+"""
+
+import cv2
+#numpy is a python library used for fast computation of arrays and in preprocessing multidimensional arrays
+import numpy as np 
+
+img = cv2.imread("car2.png")
+
+#now detect the edges
+edges = cv2.Canny(img, 200, 300)
+
+cv2.imwrite("edges_car.jpg", edges)
+
+cv2.imshow("edges", cv2.imread("edges_car.jpg"))    
+
+
+#displays the original image
+cv2.imshow("original image", img)
+
